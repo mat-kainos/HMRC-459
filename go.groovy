76fd5ffc5ff3e6aaeb88f8f4ae83f8faf889b730
mat-kainos <a@a.com>
2020-10-22 12:20:06 +0200

    a

---
+++ go.groovy
@@ -0,0 +1,25 @@
+#!/usr/bin/env groovy
+
+pipeline {
+    stages {
+        stage('branch name') {
+            steps {
+                sh "echo $BRANCH_NAME"
+            }
+        }
+        stage('Check condition') {
+            when {
+                    beforeAgent true
+                    expression { env.BRANCH_NAME ==~ /(?i:.*_HF)|develop/  } //matches: develop or *_HF or *_hf
+            }
+            steps {
+                sh "echo $BRANCH_NAME"
+            }
+        }
+    }
+    post {
+        always {
+            deleteDir()
+        }
+    }
+}
